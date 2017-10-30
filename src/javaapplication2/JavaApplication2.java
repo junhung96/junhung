3c89e2f50168c9418a397fdddd63471736d01319
junhung96 <33015178+junhung96@users.noreply.github.com>
2017-10-30 12:49:54 +0800

    [Finish#152385707]

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -15,6 +15,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("haha");
         // TODO code application logic here
     }
     
